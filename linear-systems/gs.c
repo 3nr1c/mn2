d138fdb5c246827e1cf801a1467888b51beb7319
Enric Florit <efz1005@gmail.com>
2018-10-24 00:49:29 +0200

    Error based stopping

--- linear-systems/gs.c
+++ linear-systems/gs.c
@@ -3,6 +3,19 @@
 #include <math.h>
 
 #define NUM 1000000
+#define MAX_ERROR 10e-12
+
+double norma_inf_vect(double *v) {
+	int i = 0;
+
+	double max = 0;
+
+	for (i = 0; i < NUM; i++) {
+		if (fabs(v[i]) > max) max = fabs(v[i]);
+	}
+
+	return max;
+}
 
 void fillB_J(double *b) 
 {
@@ -28,6 +41,7 @@ int main()
 	double *x_k = calloc(NUM, sizeof(double));
 	double *x_k_1 = malloc(NUM * sizeof(double));
 	double *b = malloc(NUM * sizeof(double));
+	double *error_vect = calloc(NUM, sizeof(double));
 	double divider;
 	int i,k;
 
@@ -36,10 +50,14 @@ int main()
 	// create b vector
 	fillB_J(b);
 
+	// set the norm to 1 to avoid skipping any iterations
+	error_vect[0] = 1;
+
 	// printf("Vector b initialized\n");
 
 	// actual iteration
-	for (k = 0; k < 200; k++) {
+	k = 0;
+	while (2 * norma_inf_vect(error_vect) >= MAX_ERROR && ++k) {
 		// printf("Starting iteration %d\n", k+1);
 
 		for (i = 0; i < NUM; i++) {
@@ -59,6 +77,7 @@ int main()
 			if (i == 1)
 				x_k_1[i] += -x_k[NUM - 1] / divider;
 
+			error_vect[i] = x_k_1[i] - x_k[i];
 		}
 		//printf("Finished iteration %d with increment %.13lf\n", k+1, error);
 
