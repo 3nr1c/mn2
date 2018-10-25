098d4c04effe0b53ed5463540ea7d5338a03ca7d
Enric Florit <efz1005@gmail.com>
2018-10-25 15:54:55 +0200

    Add test sor

--- linear-systems/test.sh
+++ linear-systems/test.sh
@@ -5,8 +5,11 @@ echo "- Testing 10"
 ./linear 10
 diff jacobi.txt test-assets/10_test.txt
 diff gs.txt test-assets/10_test.txt
+diff sor.txt test-assets/10_test.txt
 
 echo "- Testing 10e6"
 ./linear
 shasum -a 256 b.txt | diff test-assets/b_sha256.txt -
 diff jacobi.txt gs.txt
+diff jacobi.txt sor.txt
+diff gs.txt sor.txt
