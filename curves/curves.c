0a9cba5e24f1440addbe274ac47d18b0eb956f2b
Enric Florit <efz1005@gmail.com>
2018-12-21 13:04:08 +0100

    debugging, still

--- curves/curves.c
+++ curves/curves.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
+
+#define TOL 1e-10
 
 double f(double x, double y)
 {
@@ -34,16 +37,64 @@ void grad_f(double x, double y, double *result)
 	result[1] = df_y(x, y);
 }
 
-double first_approx(double start)
+double first_approx_x(double x0)
 {
-	double xi = start;
 	int n = 20;
+	double x1, error;
 	
-	while (--n > 0) { // TODO: fine control of the error
-		xi = xi - f(xi, 0) / df_x(xi, 0);
-	}
+	do {
+		x1 = x0 - f(x0, 0) / df_x(x0, 0);
+		error = x1 - x0;
+		x0 = x1;
+	} while (fabs(error) >= TOL && --n > 0);
 
-	return xi;
+	return x1;
+}
+
+double first_approx_y(double y0)
+{
+	int n = 20;
+	double y1, error;
+	
+	do {
+		y1 = y0 - f(0, y0) / df_y(0, y0);
+		error = y1 - y0;
+		y0 = y1;
+	} while (fabs(error) >= TOL && --n > 0);
+
+	return y1;
+}
+
+void newton_step(double x0, double y0, 
+				 double x1, double y1, 
+				 double h, double *result)
+{
+	double H[2];
+	double detH;
+	double *partial_f = (double*)calloc(2, sizeof(double));
+	double x2, y2;
+	int n = 1000;
+	
+	do {
+		H[0] = f(x1, y1);
+		H[1] = (x1 - x0)*(x1 - x0) + (y1 - y0)*(y1 - y0) - h*h;
+		grad_f(x1, y1, partial_f);
+
+		detH = 2*(y1 - y0)*partial_f[0] - 2*(x1 - x0)*partial_f[1];
+
+		x2 = x1 - (2*(y1 - y0)*H[0] - partial_f[1]*H[1]) / detH;
+		y2 = y1 - (-2*(x1 - x0)*H[0] + partial_f[0]*H[1]) / detH;
+
+		x1 = x2;
+		y1 = y2;
+	} while (fabs(f(x1,y1)) >= TOL 
+		   && fabs((x1 - x0)*(x1 - x0) + (y1 - y0)*(y1 - y0) - h*h) >= TOL 
+		   && --n > 0);
+
+	free(partial_f);
+
+	result[0] = x1;
+	result[1] = y1;
 }
 
 /** BEGIN TEST **/
@@ -59,7 +110,7 @@ void test()
 	assert(temp[0] == 20.8);
 	assert(temp[1] == 25.6);
 
-	assert(first_approx(0) - 0.326344025693 < 1e-10);
+	assert(first_approx_x(0) - 0.326344025693 < 1e-10);
 
 	free(temp);
 }
@@ -70,5 +121,59 @@ int main()
 	/** BEGIN TEST **/
 	test();
 	/** END TEST **/
+	double delta = 0.1;
+	double firstx = first_approx_x(0);
+	double firsty = first_approx_y(-3);
+	double *results = (double *)calloc(2, sizeof(double));
+	double *yprime = (double *)calloc(2, sizeof(double));
+	double *prev = (double *)calloc(2, sizeof(double));
+	double norm;
+	FILE* output = fopen("results.txt", "w");
+
+	results[0] = firstx;
+	results[1] = 0;
+
+	//results[0] = 0;
+	//results[1] = firsty;
+
+	//yprime[0] = delta/2;
+	//yprime[1] = 0;
+
+	printf("%.12lf %.12lf\n", results[0], results[1]);
+	fprintf(stderr, "(%.12lf, %.12lf)\n", results[0], results[1]);
+	for (int i = 0; i < 10000; i++) {
+		/*yprime[0] = (results[0] - prev[0]);
+		yprime[1] = (results[1] - prev[1]);
+		norm = sqrt(yprime[0]*yprime[0] + yprime[1]*yprime[1]);
+		yprime[0] = delta*yprime[0]/norm;
+		yprime[1] = delta*yprime[1]/norm;*/
+
+		yprime[0] = -df_y(results[0], results[1]);
+		yprime[1] = df_x(results[0], results[1]);
+		norm = sqrt(yprime[0]*yprime[0] + yprime[1]*yprime[1]);
+		yprime[0] = delta*yprime[0]/norm;
+		yprime[1] = delta*yprime[1]/norm;
+
+		fprintf(stderr, "y' = (%.6lf, %.6lf); ", yprime[0], yprime[1]);
+
+		prev[0] = results[0];
+		prev[1] = results[1];
+
+		newton_step(results[0], results[1],
+					results[0] + yprime[0], results[1] + yprime[1],
+					delta, results);
+		fprintf(stderr, "(%.12lf, %.12lf)\n", results[0], results[1]);
+		if (results[0] != results[0]
+			|| results[1] != results[1]
+			|| fabs(results[0])+fabs(results[1]) > 100) {
+			fprintf(stderr, "\nHALT AT %d STEPS\n", i);
+			break;
+		}
+		fprintf(output, "%.12lf %.12lf\n", results[0], results[1]);
+	}
+
+	fclose(output);
+	free(results);
+
 	return 0;
 }
